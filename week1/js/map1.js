56a32d51d3f6df6c38d86c0f08976623e55bb0fb
Michelle Zhao <michelle.yn.zhao@gmail.com>
2024-06-27 18:47:18 -0700

    add profile and page navigation

---
+++ week1/js/map1.js
@@ -0,0 +1,13 @@
+const map = new maplibregl.Map({
+    container: 'map', // container ID
+    style: 'https://api.maptiler.com/maps/streets/style.json?key=Oo1CDYXThbnZ95OKpxHl', // Your style URL
+    center: [-121.9622, 37.5489], // Starting position [lng, lat]
+    zoom: 15 // Starting zoom level
+});
+
+// Add a marker to the map
+new maplibregl.Marker()
+    .setLngLat([-121.9622, 37.5489])
+    .setPopup(new maplibregl.Popup({ offset: 25 }) // Add popups
+        .setHTML('Lake Elizabeth -- the local lake + city park!'))
+    .addTo(map);

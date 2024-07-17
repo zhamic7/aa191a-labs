a42537c927257ab752cb956f58a89be4b4ff1ae8
Michelle Zhao <michelle.yn.zhao@gmail.com>
2024-07-17 07:22:41 -0700

    lab3

--- week3/js/init.js
+++ week3/js/init.js
@@ -32,66 +32,55 @@ function createCustomIcon (caption, latlng) {
         .setHTML(caption));
 }
 
-function Place(place_name, place_district, place_city, place_desc, lat, lon) {
+function Place(index, place_name, place_district, place_city, place_desc, lat, lon) {
+    let image_url = "gallery.html#image_" + index;
     this.caption = `<div class="mapPopup">
                         <div class="popupTitle">
                             <h2 style="font-size:16px;">${place_name}, ${place_district}, ${place_city}</h2>
                         </div>
-                        <p style="font-size:15px;">${place_desc}</p>
+                        <a style="font-size: 13px;" href="${image_url}"}>View Image >></a>
+                        <p style="margin-top: 7px; font-size: 15px;">${place_desc}</p>
                     </div>`;
     this.coords = [lon, lat];
     createButtons(place_city, lat, lon, 9);
 }
 
-const places = [
-    new Place(
-        'Zhujiajiao',
-        'Qingpu District',
-        'Shanghai', 
-        'Super pretty water town with lots of historic architecture and yummy food.',
-        31.113630863554462,
-        121.05345698483865
-    ),
-    new Place(
-        'Leifeng Pagoda',
-        'Xihu District',
-        'Hangzhou',
-        'Originally a Buddhist tower that has been rebuilt over the years. \
-            I sadly didn\'t have time to go see the view from the topmost level.',
-        30.231512056688324,
-        120.149705725118
-    ),
-    new Place(
-        'Classical Gardens of Suzhou',
-        'Gusu District',
-        'Suzhou',
-        'Collection of gardens. The lotus ponds were really pretty!',
-        31.372395575344928,
-        120.62852505160254
-    ),
-    new Place(
-        'Nanjing Fuzimiao',
-        'Qinhuai District',
-        'Nanjing',
-        'A Confucius Temple where the imperial exam was held. \
-            There\'s a museum on the same street with a cheatsheet from the Qing Dynasty \
-            on display. As expected, the handwriting is tiny, haha.',
-        32.02184144417682,
-        118.79184414016119
-    ),
-]
+function processData (data) {
+    let places = [];
+    for (let i = 0; i < data.features.length; i++) {
+        let tmp = data.features[i];
+        places.push(new Place(i + 1,
+                                tmp.properties.name,
+                                tmp.properties.district,
+                                tmp.properties.city,
+                                tmp.properties.desc,
+                                tmp.geometry.coordinates[1],
+                                tmp.geometry.coordinates[0]
+        ));
+    }
+    places.forEach(place => createCustomIcon(place.caption, place.coords).addTo(map) );
+}
+
+let initZoom = {
+    "lat": 31.204359154022054,
+    "lon": 120.10623118282591,
+    "zoom": 7
+}
 
 // Create map and add markers
 const map = new maplibregl.Map({
     container: 'map', // container ID
     style: 'https://api.maptiler.com/maps/dataviz/style.json?key=aRyJfnj25CKYN2noi9Wt', // Your style URL
-    center: [120.10623118282591, 31.204359154022054], // Starting position [lng, lat]
-    zoom: 7 // Starting zoom level
+    center: [initZoom.lon, initZoom.lat], // Starting position [lng, lat]
+    zoom: initZoom.zoom // Starting zoom level
 });
-createButtons("Default View", 31.204359154022054, 120.10623118282591, 7);
+createButtons("Default View", initZoom.lat, initZoom.lon, initZoom.zoom);
 
-let markers = [];
-for (let i = 0; i < places.length; i++) {
-    markers.push(createCustomIcon(places[i].caption, places[i].coords));
-}
-markers.forEach((marker) => marker.addTo(map)); // Ensure the order is longitude, latitude
+map.on("load", function() {
+    fetch("map.geojson") // fetch data
+    .then(data => { return data.json(); }) // check data
+    .then(data => { processData(data); }) // process data
+    .catch(function(error) {
+        console.log("An error occurred: ", error);
+    });
+});
